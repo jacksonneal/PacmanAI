08e4f4807defc99e81ecd0a7199a96a68034e2f8
jacksonneal <neal.ja@husky.neu.edu>
2020-10-25 18:34:51 -0400

    undo

--- unpack.py
+++ unpack.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -12,23 +12,26 @@
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
-import os, cPickle, sys
+import os
+import cPickle
+import sys
 
 if len(sys.argv) != 3:
-  print 'Usage: %s stats_file team_name' % sys.argv[0]
-  print 'Unpacks the stats file of a server into a bunch of replay files.'
-  if len(sys.argv) == 2:
-    d = cPickle.load(open(sys.argv[1]))
-    print 'Team names:', d.keys()
-  sys.exit(2)
+    print('Usage: %s stats_file team_name' % sys.argv[0])
+    print('Unpacks the stats file of a server into a bunch of replay files.')
+    if len(sys.argv) == 2:
+        d = cPickle.load(open(sys.argv[1]))
+        print('Team names:', d.keys())
+    sys.exit(2)
 
 d = cPickle.load(open(sys.argv[1]))
 user = sys.argv[2]
 k = 0
-print 'Unpacking games for', user
+print('Unpacking games for', user)
 for g, w in d[user]['gameHistory']:
     k += 1
-    t = {'layout': g.state.data.layout, 'agents': g.agents, 'actions': g.moveHistory, 'length': g.length}
+    t = {'layout': g.state.data.layout, 'agents': g.agents,
+         'actions': g.moveHistory, 'length': g.length}
     fname = 'replay_' + user + '_' + str(k)
-    print 'Game:', fname
-    cPickle.dump(t,file(fname, 'w'))
+    print('Game:', fname)
+    cPickle.dump(t, file(fname, 'w'))
