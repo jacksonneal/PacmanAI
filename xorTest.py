e3d2a18f4204440d27ccefa9665cfc01304e6def
The_Number_e <edwardx999@gmail.com>
2020-11-23 12:26:52 -0500

    test xor with add_connection

--- xorTest.py
+++ xorTest.py
@@ -16,9 +16,13 @@ class XorFitness:
                     error += diff * diff
                 ind.setFitness(-error)            
 
-base = Genes(2, 1, Genes.Metaparameters(perturbation_chance=0.5, perturbation_stdev=0.5, new_link_weight_stdev=1))
+base = Genes(2, 1, Genes.Metaparameters(perturbation_chance=0.5, perturbation_stdev=0.5, new_link_weight_stdev=4, c1=2, c2=2, c3=0.4))
 population = [base.clone() for i in range(150)]
-optimizer = GeneticOptimizer(population, XorFitness(), 1000)
+for ind in population:
+    ind.add_connection(ind.input_node_index(0), ind.output_node_index(0))
+    ind.add_connection(ind.input_node_index(1), ind.output_node_index(0))
+    ind.add_connection(Genes.BIAS_INDEX, ind.output_node_index(0))
+optimizer = GeneticOptimizer(population, XorFitness(), 200)
 optimizer.initialize()
 optimizer.evolve()
 best = optimizer.getBestIndividual()
@@ -27,3 +31,4 @@ for input, output in zip(inputs, outputs):
     neurons = best.feed_sensor_values(input)
     result = best.extract_output_values(neurons)[0]
     print(f"{input[0]} xor {input[1]} = {result}")
+pass
