944f8026bdd2fc539f7aeb297c2ba618f66cd1f1
The_Number_e <edwardx999@gmail.com>
2020-11-25 00:10:48 -0500

    atari boxing

---
+++ atari_help.pyx
@@ -0,0 +1,19 @@
+import numpy as np
+
+def run_game(env, ind, render, time_limit=2000):
+    fitness = 0
+    observation = env.reset()
+    neurons = None
+    for t in range(time_limit):
+        if render:
+            env.render()
+        neurons = ind.feed_sensor_values(observation, neurons)
+        result = ind.extract_output_values(neurons)
+        action = np.argmax(result)
+        observation, reward, done, info = env.step(action)
+        fitness += reward
+        if done or fitness < -10:
+            break
+    env.close()
+    print(f"{fitness}")
+    return fitness
