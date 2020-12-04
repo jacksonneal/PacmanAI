63e6be22b2d5c4b6284af1a10dd9bc363598534a
The_Number_e <edwardx999@gmail.com>
2020-12-03 19:38:04 -0500

    positive xor fitness

--- xorTest.py
+++ xorTest.py
@@ -14,7 +14,7 @@ class XorFitness:
                     result = ind.extract_output_values(neurons)[0]
                     diff = result - output
                     error += diff * diff
-                ind.setFitness(-error)            
+                ind.setFitness(4 - error)            
 
 base = Genes(2, 1, Genes.Metaparameters(perturbation_chance=0.5, perturbation_stdev=0.5, new_link_weight_stdev=4, c1=2, c2=2, c3=0.4))
 population = [base.clone() for i in range(150)]
@@ -22,7 +22,7 @@ for ind in population:
     ind.add_connection(ind.input_node_index(0), ind.output_node_index(0))
     ind.add_connection(ind.input_node_index(1), ind.output_node_index(0))
     ind.add_connection(Genes.BIAS_INDEX, ind.output_node_index(0))
-optimizer = GeneticOptimizer(population, XorFitness(), 60)
+optimizer = GeneticOptimizer(population, XorFitness(), 100)
 optimizer.initialize()
 optimizer.evolve()
 best = optimizer.getBestIndividual()
