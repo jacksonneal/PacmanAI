8a376e14882251957c027ece358ea525576fb1ea
The_Number_e <edwardx999@gmail.com>
2020-12-10 23:13:27 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -2,6 +2,23 @@ from genes import *
 import numpy as np
 import time
 
+meta = Genes.Metaparameters()
+mj = meta.as_json()
+g = Genes(5, 5, meta)
+neurons = None
+input = [0, 1, 2, 3, 4, 5]
+neurons = g.feed_sensor_values(input, neurons)
+for _ in range(20):
+    g.mutate()
+out = g.as_json()
+g2 = g.clone()
+meta.reset_tracking()
+for _ in range(20):
+    g.mutate()
+out2 = g2.as_json()
+out3 = g.as_json()
+pass
+"""
 f = open("atari_pacman6/sample_gene_446.json", "r")
 genes = Genes.load(f, Genes.Metaparameters())
 f.close()
@@ -17,5 +34,6 @@ for _ in range(20000):
 t2 = time.time()
 print(f"Time = {t2 - t1}")
 pass
-
+"""
+pass
 
